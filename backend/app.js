1cd985010fbf12fb0a2bd61f3ff7b53b2a178ec0
martin <martin.aubertin@90tech.fr>
2023-12-11 12:38:36 +0100

    edit atch method

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,25 @@ const bcrypt = require('bcrypt');
 const app = express(); 
 const mongoose = require('./database/mongoose');
 const jwt = require("jsonwebtoken");
+const multer = require('multer');
+const path = require('path');
 require("dotenv").config();
 
 const Tattoos = require('./database/models/tattoos');
 const User = require('./database/models/users');
 
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../frontend/src/assets/img/tattoos'));
+  },
+  filename: function (req, file, cb) {
+    const uniqueFileName = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueFileName);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 // enable CORS Cross Origin Request Security
 app.use((req, res, next) => { 
@@ -16,11 +30,6 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-
-// localhost:3000 - backend api 
-
-//frontend - localhost:4200
 
 
 app.use(express.json());
@@ -42,20 +51,36 @@ app.get("/tattoos/:tattoosId", (req, res) => {
     .catch(error => res.json(error))
 })
 
-app.post("/tattoos", (req, res) => { 
-    new Tattoos({ 
-        'name': req.body.name,
-        'description': req.body.description
-    }).save()
-    .then(data => res.json(data))
-    .catch(error => res.json(error))
+app.post("/tattoos", upload.single('image'), (req, res) => {
+  const { name, description } = req.body;
+  const imagePath = req.file.originalname;
+  const fullImagePath = `assets/img/tattoos/${req.file.filename}`;
+
+  new Tattoos({ 
+    name: name,
+    description: description,
+    img: fullImagePath
+  }).save()
+  .then(data => res.json(data))
+  .catch(error => res.json(error))
 });
 
-app.patch('/tattoos/:tattoosId', (req, res) => { 
-    Tattoos.findOneAndUpdate({_id: req.params.tattoosId}, {$set: req.body}) 
-        .then(data => res.json(data))
-        .catch(error => res.json(error))
+app.patch('/tattoos/:tattoosId', upload.single('image'), (req, res) => {
+  const { name, description } = req.body;
+  const existingImagePath = req.body.img; // Conserve l'image existante si elle est définie
+  const fullImagePath = req.file ? `assets/img/tattoos/${req.file.filename}` : existingImagePath;
+  const updateFields = {
+    img: fullImagePath,
+    name: name,
+    description: description
+  };
+
+  Tattoos.findOneAndUpdate({ _id: req.params.tattoosId }, { $set: updateFields }, { new: true })
+    .then(data => res.json(data))
+    .catch(error => res.json(error));
 });
+
+
 
 app.delete('/tattoos/:tattoosId', (req, res) => { 
     Tattoos.deleteOne({"_id": req.params.tattoosId})
@@ -64,7 +89,6 @@ app.delete('/tattoos/:tattoosId', (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-
     try {
       // Get user input
       const { email, password } = req.body;
