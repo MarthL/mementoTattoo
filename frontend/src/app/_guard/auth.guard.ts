87506da645c57a745168df6dd03b2a39c3e74c6d
martin <martin.aubertin@90tech.fr>
2023-12-28 16:07:37 +0100

    migration 16 2/2

--- frontend/src/app/_guard/auth.guard.ts
+++ frontend/src/app/_guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import Swal from 'sweetalert2';
 import { AuthServiceService } from '../_services/auth-service.service';
@@ -7,7 +7,7 @@ import { AuthServiceService } from '../_services/auth-service.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
